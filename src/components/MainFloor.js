4c9546db4b3cea1102533cb153f77593fe74b4e0
Hyojoong0607 <erickim06071@gmail.com>
2020-06-24 16:03:42 -0700

    MainFloor Added

---
+++ src/components/MainFloor.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './MainFloor.css';
+
+
+const MainFloor = () => {
+
+    return (
+        <div className="parent">
+            <div className="div1">Reception</div>
+            <div className="div2">Touch Tank</div>
+            <div className="div3">Kid's Section</div>
+            <div className="div4">Shark Tank</div>
+            <div className="div5">Fish Tank</div>
+            <div className="div6">Stars</div>
+            <div className="div7">Lab/Research Room</div>
+        </div>
+    )
+}
+
+export default MainFloor;
