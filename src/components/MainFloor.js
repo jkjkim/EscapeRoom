021d282282b047112ba59fb5cde655aae6ff3499
Hyojoong0607 <erickim06071@gmail.com>
2020-07-13 16:26:45 -0700

    Updating MainFloor

--- src/components/MainFloor.js
+++ src/components/MainFloor.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './MainFloor.css';
 import Touch from '../data/TouchTank.png'
-
+import Shark from '../data/Shark Tank Clipart.png'
 const MainFloor = () => {
 
     return (
         <div className="parent">
             <div className="div1">Stars</div>
-            <div className="div2">Shark Tank</div>
+            <div className="div2">Shark Tank
+            <img style={{ left:"40%", top:"50%", position: "absolute" }} src={Shark} width="200" alt="imgs" />           
+            </div>
             <div className="div3">Reception</div>
             <div className="div4"> Touch Tank
-                <img src={Touch} width="200" height="300" alt="1" />
+            <img style={{ left:"40%", top:"50%", position: "absolute" }} src={Touch} width="200" alt="imgs" />           
             </div> 
             <div className="div5">Fish Tank</div>
             <div className="div6">Kid's Section</div>
