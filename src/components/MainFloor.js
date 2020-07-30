e701ced1e9c4c429b0a18d2e6176dd6dc314cbf1
Hyojoong0607 <erickim06071@gmail.com>
2020-07-29 17:00:31 -0700

    Mainfloor

--- src/components/MainFloor.js
+++ src/components/MainFloor.js
@@ -6,17 +6,23 @@ import SharkImg from '../data/Shark Tank Clipart.png'
 import KidsImg from '../data/Gate_Final.png'
 import ResearchImg from '../data/Door.png'
 import FishImg from '../data/TempFishTank.png'
+import ReceptionImg from '../data/Podium_Final.png'
+import TempStars from '../data/TempStars.PNG'
 const MainFloor = () => {
 
     return (
         <div className="parent">
-            <div className="div1">Stars</div>
+            <div className="div1">
+            <img style={{position: "absolute" }} src={TempStars} width="2000" height="150" alt="imgs" />     
+            </div>
             <div className="div2">
                 <Link to = '/shark'>
                     <img style={{position: "absolute" }} src={SharkImg} width="300" alt="imgs" />
                 </Link>
             </div>
-            <div className="div3">Reception</div>
+            <div className="div3">
+            <img style={{position: "absolute" }} src={ReceptionImg} width="200" alt="imgs" />   
+            </div>
             <div className="div4">
             <Link to = '/touchtank'>
                 <img style={{position: "absolute" }} src={TouchImg} width="300" alt="imgs" />           
